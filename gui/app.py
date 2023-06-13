414acd5e0e39e4755e0e7127aba42fe7aaf716a8
Sunil16666 <79449240+Sunil16666@users.noreply.github.com>
2023-06-13 22:48:57 +0200

    test CTk

---
+++ gui/app.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+import customtkinter as ctk
+from components.widget import MyWidget
+
+
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("dark-blue")
+
+
+class App:
+    def __init__(self):
+        self.root = ctk.CTk()
+        self.root.title("FLATBOT")
+        self.root.geometry("800x600")
+        self.root.resizable(width=True, height=True)
+
+        self.create_widgets()
+
+    def create_widgets(self):
+        # Create an instance of the custom widget
+        my_widget = MyWidget(self.root)
+        my_widget.pack()
+
+    def run(self):
+        self.root.mainloop()
+
+
+if __name__ == "__main__":
+    app = App()
+    app.run()
